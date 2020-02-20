3f3a58fb51dfb59c1da34f4a18990cf2e47fef1d
xiaobo.kong <xiaobo.kong@aliyun.com>
2020-02-20 15:49:54 +0800

    readonly property demo

---
+++ 06_class/property.py
@@ -0,0 +1,33 @@
+class Account(object):
+    """账号类,
+    amount 是美元金额
+    """
+
+    def __init__(self,rate):
+        self.__amt = 0
+        self.rate = rate
+
+    @property
+    def amount(self):
+        """ 账号金额(美元) """
+        return self.__amt
+
+    @property
+    def cny(self):
+        """ 账号金额(人民币) """
+        return self.__amt * self.rate
+
+    @amount.setter
+    def amount(self, value):
+        if value < 0:
+            print("Sorry,no negative amount in the account.")
+            return
+        self.__amt = value
+
+if __name__ == '__main__':
+    acc = Account(rate=6.6)
+    acc.amount = 18
+    print("Dollar amount:",acc.amount)
+    print("In CNY:",acc.cny)
+    acc.amount = -100
+    print("Dollar amount:",acc.amount)
