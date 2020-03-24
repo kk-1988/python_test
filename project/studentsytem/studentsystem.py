5ee18c7fb7599d06b37dca10b380d2a4168ca5f2
xiaobo.kong <xiaobo.kong@broadlink.com.cn>
2020-03-24 13:02:10 +0800

    添加delete接口

--- project/studentsytem/studentsystem.py
+++ project/studentsytem/studentsystem.py
@@ -89,3 +89,37 @@ def insert():
         save(stdentList)                #将学生信息保存到文件
         print("学生信息录入完毕!!!")
         
+def delete():
+    mark = True
+    while mark:
+        studentId = input("请输入要删除的学生ID：")
+        if studentId is not "":
+            if os.path.exist(filename):
+                with open(filename, 'r') as rfile:
+                    student_old = rfile.readlines()
+            else:
+                student_old = []
+            ifdel = False
+            if student_old:
+                with open(filename, 'w') as wfile:
+                    d = {}
+                    for list in student_old:
+                        d = dict(eval(list))
+                        if d['id'] != studentId:
+                            wfile.write(str(d) + "\n")
+                        else:
+                            ifdel = True
+                    if ifdel:
+                        print("ID为 %s 的学生信息已经被删除..." % studentId)
+                    else:
+                        print("没有找到ID为 %s 的学生信息..." % studentId)
+            else:
+                print("无学生信息...")
+                break
+            show()
+            inputMark = input("是否继续删除? (y/n) :")
+            if inputMark == "y"
+                mark = True
+            else:
+                mark = False
+                  
